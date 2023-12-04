f76e2ec7b3a9cc1114030d404e765a35165ddf99
Felipe Rodrigues <contactfeliper@gmail.com>
2023-12-04 15:05:25 -0300

    Header finalized.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import syles from './Header.module.css';
+import styles from './Header.module.css';
 import { Link } from 'react-router-dom';
+import Dogs from '/src/assets/dogs.svg?react';
 
 const Header = () => {
   return (
-    <div className={syles.header}>
-      <nav className="container">
-        <Link to="/">Home</Link>
-        <Link to="/login">Login / Criar</Link>
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link className={styles.logo} to="/" aria-label="Dogs - Hoome">
+          <Dogs />
+        </Link>
+        <Link className={styles.login} to="/login">
+          Login / Criar
+        </Link>
       </nav>
-    </div>
+    </header>
   );
 };
 
