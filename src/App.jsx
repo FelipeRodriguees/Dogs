be706680e26a1b4775ccb123e849d013c067be01
Felipe Rodrigues <contactfeliper@gmail.com>
2023-12-04 13:38:50 -0300

    esqueleto base.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Footer from './components/footer/Footer';
+import Home from './components/home/Home';
+import Header from './components/header/Header';
+import Login from './components/login/Login';
+
 function App() {
-  return <h1>Dogs</h1>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
